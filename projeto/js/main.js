b038f29a5c18363ea3bc7fb5f05ac334b76c8bcc
felipebernardes <bernardes.flp@gmail.com>
2016-07-29 20:28:29 -0300

    Adiciona smooth scroll

---
+++ projeto/js/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+  var nav = document.querySelector('nav');
+  var links = document.querySelectorAll('nav a');
+
+  console.log('e aí cara', nav, links);
+
+  $('.scroll').click(function() {
+    var target = $(this.hash);
+
+    $('html, body').animate({
+      scrollTop: target.offset().top
+    }, 500);
+
+    return false;
+  });
+});
